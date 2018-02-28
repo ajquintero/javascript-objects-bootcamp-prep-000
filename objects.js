73e2d7445fc0024e87c25b9ce9432b45cddd1923
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 20:04:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 
-function removeFromPlayList(playlist, artistName, songTitle){
+function removeFromPlayList(playlist, artistName){
   delete playlist[artistName];
   return playlist;
 }

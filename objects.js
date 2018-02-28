f8ad1a3fda0c707f08a238d1bfc8aabcf034e0a0
Abel Quintero <abel.j.quintero@gmail.com>
2018-02-28 20:03:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = { artist:'', songtitle: ''}
+var playlist = new Object({keys: '0'})
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlayList(playlist, artistName, songTitle){
+  delete playlist[artistName];
+  return playlist;
+}
